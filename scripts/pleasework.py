49a253910f4427e220f9565b08cefcaed0b2e011
HarrierPigeon <32232925+HarrierPigeon@users.noreply.github.com>
2022-09-09 22:13:25 -0500

    added step, going to try this agaiiiin

---
+++ scripts/pleasework.py
@@ -0,0 +1,8 @@
+import os
+
+input_source = "git-info.js"
+print("current working directory:",os.getcwd())
+output_dest = "aaa-git-info.js"
+print("renaming")
+os.rename(input_source,output_dest)
+print("finished")

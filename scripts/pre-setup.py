9932fd7266759b0c0401227580109fc58f6f78d5
HarrierPigeon <32232925+HarrierPigeon@users.noreply.github.com>
2022-09-10 22:45:09 -0500

    Getting MVP demo setup.

--- scripts/pre-setup.py
+++ scripts/pre-setup.py
@@ -1,7 +1,12 @@
+import os
+import shutil
 print("placeholder ATM")
 
 checklist = """
 Things to check for:
+
+Does clasp.json exist yet?
+
 Does git-info.js (or TS?) exist yet?  - if not, copy over local version
 
 Does .claspignore exist yet? - if not, create a file, and put /deploy-data in it
@@ -11,3 +16,15 @@ Could also stick an empty .gs file in there as a watermark, lol?
 
 Basically I need to find *everything* necessary to make an empty repo and push it and make a local, basic copy.
 """
+
+claspJsonPath = "clasp.json"
+
+reqFileDir = "deploy-data/required-files/"
+
+if os.path.exists(claspJsonPath) == False:
+    shutil.copy2(claspJsonPath,reqFileDir+claspJsonPath)
+
+gitInfo_path =  "git-info.js"
+if os.path.exists(gitInfo_path) == False:
+    shutil.copy2(gitInfo_path,reqFileDir+gitInfo_path)
+
